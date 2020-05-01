264e48f38e31057517e83e23b4cc0c3d0f90206f
Berbardo <bernardo.rcoutinho@gmail.com>
2020-05-01 00:54:00 -0300

    :sparkles: Introduce TD3

--- td3.py
+++ td3.py
@@ -29,7 +29,7 @@ class Critic(nn.Module):
 
         self.linear1 = nn.Linear(env.observation_space.shape[0] + env.action_space.shape[0], 400)
         self.bn1 = nn.BatchNorm1d(400)
-        self.linear2 = nn.Linear(400 + env.action_space.shape[0], 300)
+        self.linear2 = nn.Linear(400, 300)
         self.bn2 = nn.BatchNorm1d(300)
         self.linear3 = nn.Linear(300, 1)
 
@@ -42,7 +42,7 @@ class Critic(nn.Module):
         return q
 
 class TD3:
-    def __init__(self, env, alpha=0.001, beta=0.001, gamma=0.99, tau=0.005, policy_noise=0.2, noise_clip=0.5, policy_freq=2):
+    def __init__(self, env, alpha=0.0005, beta=0.0005, gamma=0.99, tau=0.005, policy_noise=0.1, noise_clip=0.3, policy_freq=2):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         self.env  = env
@@ -109,7 +109,7 @@ class TD3:
             actions.append(a)
             rewards.append([r])
             next_states.append(s2)
-            dones.append(d)
+            dones.append([d])
 
         states = torch.FloatTensor(states).to(self.device)
         actions = torch.FloatTensor(actions).to(self.device)
@@ -123,15 +123,16 @@ class TD3:
             self.update_target()
 
     def _train_critics(self, states, actions, rewards, next_states, dones):
-        noise = actions.data.normal_(0, self.policy_noise).to(device)
-        noise = noise.clamp(-self.noise_clip, self.noise_clip)
-        next_actions = (self.target_actor.forward(next_states) + noise)
-        next_actions = next_actions.clamp(-self.max_action, self.max_action)
+        with torch.no_grad():
+            noise = actions.data.normal_(0, self.policy_noise).to(self.device)
+            noise = noise.clamp(-self.noise_clip, self.noise_clip)
+            next_actions = (self.target_actor.forward(next_states) + noise)
+            next_actions = next_actions.clamp(-1, 1)
 
-        target_Q1 = self.target_critic1(next_states, next_actions)
-        target_Q2 = self.target_critic2(next_states, next_actions)
-        target_Q = torch.min(target_Q1, target_Q2)
-        target_Q = rewards + ((1-dones) * self.gamma * target_Q).detach()
+            target_Q1 = self.target_critic1(next_states, next_actions)
+            target_Q2 = self.target_critic2(next_states, next_actions)
+            target_Q = torch.min(target_Q1, target_Q2)
+            target_Q = rewards + ((1-dones) * self.gamma * target_Q).detach()
         
         current_Q1 = self.critic1(states, actions)
         loss_Q1 = F.mse_loss(current_Q1, target_Q)
@@ -139,7 +140,7 @@ class TD3:
         loss_Q1.backward()
         self.critic_optimizer1.step()
         
-        current_Q2 = self.critic_2(states, actions)
+        current_Q2 = self.critic2(states, actions)
         loss_Q2 = F.mse_loss(current_Q2, target_Q)
         self.critic_optimizer2.zero_grad()
         loss_Q2.backward()
