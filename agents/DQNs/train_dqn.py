d0ff428f26c71167b1b028739012f542d7ad915f
Berbardo <bernardo.rcoutinho@gmail.com>
2020-09-07 18:23:41 -0300

    :recycle: Start DQN refactoring

--- agents/DQNs/train_dqn.py
+++ agents/DQNs/train_dqn.py
@@ -1,20 +1,24 @@
 import gym
+import matplotlib.pyplot as plt
 
 from dqn import DQN
 from ddqn import DDQN
-from dueling_ddqn import DuelingDDQN
-from noisy_dqn import NoisyDQN
-from categorical_dqn import CategoricalDQN
-from rainbow import Rainbow
+# from dueling_ddqn import DuelingDDQN
+# from noisy_dqn import NoisyDQN
+# from categorical_dqn import CategoricalDQN
+# from rainbow import Rainbow
 
-from utils.dqn_runner import vector_train
+from utils.dqn_runner import train
 from utils.dqn_runner import evaluate
 
 if __name__ == "__main__":
-    env = gym.vector.make("CartPole-v1", num_envs=4, asynchronous=True)
-    agent = DDQN(env.single_observation_space, env.single_action_space)
+    env = gym.make("CartPole-v1")
+    agent = DDQN(env.observation_space, env.action_space)
 
-    returns = vector_train(agent, env, 50000, 450)
+    returns = train(agent, env, 25000)
 
     eval_env = gym.make("CartPole-v1")
-    evaluate(agent, eval_env, 1, True)
+    evaluate(agent, eval_env, 10, True)
+
+    plt.plot(returns, 'r')
+    plt.show(block=True)
--- agents/DQNs/utils/experience_replay.py
+++ agents/DQNs/utils/experience_replay.py
@@ -1,9 +1,17 @@
-import torch
 import numpy as np
 
-class NumpyReplay:
-    def __init__(self, max_length, observation_space, device):
-        self.device = device
+class ReplayBuffer:
+    """Experience Replay Buffer para DQNs."""
+    def __init__(self, max_length, observation_space):
+        """Cria um Replay Buffer.
+
+        Parâmetros
+        ----------
+        max_length: int
+            Tamanho máximo do Replay Buffer.
+        observation_space: int
+            Tamanho do espaço de observação.
+        """
         self.index, self.size, self.max_length = 0, 0, max_length
 
         self.states = np.zeros((max_length, observation_space), dtype=np.float32)
@@ -12,19 +20,58 @@ class NumpyReplay:
         self.next_states = np.zeros((max_length, observation_space), dtype=np.float32)
         self.dones = np.zeros((max_length), dtype=np.float32)
 
-    def update(self, states, actions, rewards, next_states, dones):
-        self.states[self.index] = states
-        self.actions[self.index] = actions
-        self.rewards[self.index] = rewards
-        self.next_states[self.index] = next_states
-        self.dones[self.index] = dones
+    def __len__(self):
+        """Retorna o tamanho do buffer."""
+        return self.size
+
+    def update(self, state, action, reward, next_state, done):
+        """Adiciona uma experiência ao Replay Buffer.
+
+        Parâmetros
+        ----------
+        state: np.array
+            Estado da transição.
+        action: int
+            Ação tomada.
+        reward: float
+            Recompensa recebida.
+        state: np.array
+            Estado seguinte.
+        done: int
+            Flag indicando se o episódio acabou.
+        """
+        self.states[self.index] = state
+        self.actions[self.index] = action
+        self.rewards[self.index] = reward
+        self.next_states[self.index] = next_state
+        self.dones[self.index] = done
+        
         self.index = (self.index + 1) % self.max_length
         if self.size < self.max_length:
             self.size = self.index
             
     def sample(self, batch_size):
+        """Retorna um batch de experiências.
+        
+        Parâmetros
+        ----------
+        batch_size: int
+            Tamanho do batch de experiências.
+
+        Retorna
+        -------
+        states: np.array
+            Batch de estados.
+        actions: np.array
+            Batch de ações.
+        rewards: np.array
+            Batch de recompensas.
+        next_states: np.array
+            Batch de estados seguintes.
+        dones: np.array
+            Batch de flags indicando se o episódio acabou.
+        """
+        # Escolhe índices aleatoriamente do Replay Buffer
         idxs = np.random.randint(0, self.size, size=batch_size)
 
-        return (torch.as_tensor(self.states[idxs]).to(self.device), torch.as_tensor(self.actions[idxs]).to(self.device),
-                torch.as_tensor(self.rewards[idxs]).to(self.device), torch.as_tensor(self.next_states[idxs]).to(self.device),
-                torch.as_tensor(self.dones[idxs]).to(self.device))
+        return (self.states[idxs], self.actions[idxs], self.rewards[idxs], self.next_states[idxs], self.dones[idxs])
